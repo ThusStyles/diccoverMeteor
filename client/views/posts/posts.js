ecdd8d9a60c1af75f43bf3b8948618b0448b6e8c
Theo Styles <thus_styles@msn.com>
2014-05-28 19:27:49 +0100

    updated fixtures

--- client/views/posts/posts.js
+++
@@ -1,9 +0,0 @@
-Template.postsList.helpers({
-	postsWithRank: function(){
-		this.posts.rewind();
-		return this.posts.map(function(post, index, cursor){
-			posts._rank = index;
-			return post;
-		});
-	}
-})
--- server/fixtures.js
+++ server/fixtures.js
@@ -3,19 +3,31 @@ if (Posts.find().count() === 0) {
 	Posts.insert({
 		title: 'Introducing Telescope',
 		author: 'Sacha Greif',
-		url: 'http://sachagreif.com/introducing-telescope/'
+		url: 'http://sachagreif.com/introducing-telescope/',
+		submitted: new Date().getTime(),
+		commentsCount: 0,
+		votes: 0,
+		upvoters: []
 	});
 
 	Posts.insert({
 		title: 'Meteor',
 		author: 'Tom Coleman',
-		url: 'http://meteor.com'
+		url: 'http://meteor.com',
+		submitted: new Date().getTime(),
+		commentsCount: 0,
+		votes: 0,
+		upvoters: []
 	});
 
 	Posts.insert({
 		title: 'The Meteor Book',
 		author: 'Tom Coleman',
-		url: 'http://themeteorbook.com'
+		url: 'http://themeteorbook.com',
+		submitted: new Date().getTime(),
+		commentsCount: 0,
+		votes: 0,
+		upvoters: []
 	});
 	
 }
